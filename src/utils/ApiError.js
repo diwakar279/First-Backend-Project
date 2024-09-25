7b0d339045a8557b121be842eb4ebefe799e8bda
Thakur Diwakar Suryawanshi <balramthakur1973@gmail.com>
2024-09-25 13:03:01 +0530

    else error solve

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,29 +1,23 @@
-
-class ApiError extends Error{
-    constructor(
-        stack="",
-        errors=[],
-        message="Something went wrong",
-        statusCode
-    ){
-        super(message),
-        this.errors=errors,
-        this.data=null,
-        this.message=message,
-        this.statusCode=statusCode,
-        this.success=false    
-    }
-
+class ApiError extends Error {
+  constructor(
+    stack = "",
+    errors = [],
+    message = "Something went wrong",
+    statusCode
+  ) {
+    super(message),
+      (this.errors = errors),
+      (this.data = null),
+      (this.message = message),
+      (this.statusCode = statusCode),
+      (this.success = false);
 
     //Below code is used to track errors
+
     if (stack) {
-        this.stack=stack
-    } 
-    //This code is giving me error I don't know why
-    // else{
-    //     Error.captureStackTrace(this, this.constructor)
-    // }
-  
-
-
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
 }
